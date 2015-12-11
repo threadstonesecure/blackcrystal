bc391f931dd60648e80920e4d2cff4e94c1d88dc
Engin Yöyen <engin.yoyen@gmail.com>
2015-12-11 22:48:15 +0100

    Clean up scheduler and runner

--- src/main/java/blackcrystal/runner/Runner.java
+++ src/main/java/blackcrystal/runner/Runner.java
@@ -1,28 +1,39 @@
-package blackcrystal.runner.component;
+package blackcrystal.runner;
 
-import blackcrystal.data.domain.JobConfig;
-import blackcrystal.runner.domain.Queue;
+import blackcrystal.model.JobConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
+import java.util.concurrent.Callable;
 
 
 @Component
-public class Runner implements Runnable {
+public class Runner implements Runnable, Callable {
 
+    private static final Logger logger = LoggerFactory
+            .getLogger(Runner.class);
 
-    public Runner(){
+    public Runner() {
 
     }
+
     private JobConfig jobConfig;
 
-    public Runner(Queue queue) {
-        this.jobConfig = queue.jobConfig;
+    public Runner(JobConfig jobConfig) {
+        this.jobConfig = jobConfig;
     }
 
-    public void run() {
-        Process process= null;
 
+    @Override
+    public void run() {
+        logger.debug("Running job " + jobConfig.name);
+    }
+
+
+    public void runProcess() {
+        Process process = null;
         try {
             ProcessBuilder pb = new ProcessBuilder(jobConfig.command.split(" "));
             pb.directory(new File(jobConfig.executionDirectory));
@@ -38,9 +49,9 @@ public class Runner implements Runnable {
 
         String line;
         //TODO  : Redirect stdout to log file
-        try{
-            while ((line = reader.readLine ()) != null) {
-                System.out.println (line);
+        try {
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -48,4 +59,8 @@ public class Runner implements Runnable {
 
     }
 
+    @Override
+    public Object call() throws Exception {
+        return null;
+    }
 }
---
+++ src/main/java/blackcrystal/runner/ScheduledTasks.java
@@ -0,0 +1,64 @@
+package blackcrystal.runner;
+
+import blackcrystal.app.ApplicationProperties;
+import blackcrystal.model.JobConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ScheduledFuture;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    @Autowired
+    private TaskExecutor taskExecutor;
+
+
+    @Autowired
+    private ThreadPoolTaskScheduler scheduler;
+
+
+    @Autowired
+    private ApplicationProperties applicationProperties;
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+         return scheduler;
+    }
+
+    private List<ScheduledFuture> scheduledFutures = new ArrayList<>();
+
+
+    //TODO get pool size from application config
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(50);
+        return executor;
+    }
+
+    @Bean
+    public boolean loadJobs() {
+        for(JobConfig j : applicationProperties.getJobs()){
+            ScheduledFuture s = scheduler.schedule(new Runner(j), new CronTrigger(j.executionTime));
+            scheduledFutures.add(s);
+        }
+        return true;
+    }
+    
+}
