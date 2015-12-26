4c46055c8910fe7d9c8fb942b5d502ab6b973ea0
Engin Yöyen <engin.yoyen@gmail.com>
2015-12-26 20:25:46 +0100

    Remove front-end files

--- static/ui/js/job-controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('jobController', [])
-	.controller('job', function($scope, $http) {
-		 	$http.get('http://localhost:8080/jobs').success(function(data) {
-       		$scope.jobs = data;
-   		});
-	});
-
