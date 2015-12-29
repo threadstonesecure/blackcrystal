df724c6bddbe9df2a42d15b7a0244660ad497267
Engin Yöyen <engin.yoyen@gmail.com>
2015-12-29 23:11:17 +0100

    Register Jsr310JpaConverters for converting JSR-310 types into legacy Dates

--- src/main/java/blackcrystal/Application.java
+++ src/main/java/blackcrystal/Application.java
@@ -3,12 +3,21 @@ package blackcrystal;
 import blackcrystal.app.PropertyValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/***
+ *
+ * JPA 2.1 converters to turn JSR-310 types into legacy Dates.
+ * http://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/convert/threeten/Jsr310JpaConverters.html
+ */
+@EntityScan(basePackageClasses = {Application.class, Jsr310JpaConverters.class})
 
 @SpringBootApplication
 @EnableScheduling
@@ -30,7 +39,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("PUT", "DELETE","GET","POST");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("PUT", "DELETE", "GET", "POST");
             }
         };
     }
